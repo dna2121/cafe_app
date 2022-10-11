7f1a42bb2aff74a4353556f1832eadddf1dc0704
dna2121 <dnatayawibisono@gmail.com>
2022-10-11 22:23:23 +0700

    app color

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color.fromRGBO(122, 88, 88, 1); //header
+  static const Color textColor = Color.fromRGBO(173, 139, 115, 1);
+  static const Color buttonColor = Color.fromRGBO(206, 171, 147, 1);
+  static const Color navbarColor = Color.fromRGBO(227, 202, 165, 1);
+}
