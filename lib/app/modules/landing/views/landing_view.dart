7f7c51a1b18df27238eaf9e3167fbed56d64fd20
dna2121 <dnatayawibisono@gmail.com>
2022-10-14 14:55:51 +0700

    landing page

--- lib/app/modules/landing/views/landing_view.dart
+++ lib/app/modules/landing/views/landing_view.dart
@@ -1,7 +1,9 @@
+import 'package:cafe_app/constants/app_color.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/landing_controller.dart';
 
 class LandingView extends GetView<LandingController> {
@@ -9,14 +11,48 @@ class LandingView extends GetView<LandingController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LandingView'),
-        centerTitle: true,
-      ),
       body: Center(
-        child: Text(
-          'LandingView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(36, 80, 36, 40),
+              child: Image.asset(
+                "assets/images/cafeapp.png",
+                height: 223,
+                width: 298,
+              ),
+            ),
+            const Text(
+              "ca·fé",
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w700,
+                  color: AppColor.textColor),
+            ),
+            const SizedBox(height: 171),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 58),
+              child: SizedBox(
+                height: 48,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.LOGIN);
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(AppColor.primaryColor),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(28.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text("Get Started"),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
