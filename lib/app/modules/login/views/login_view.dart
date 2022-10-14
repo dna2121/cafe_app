9c93bcb09016803d53e858eb094256c0e1f3002a
dna2121 <dnatayawibisono@gmail.com>
2022-10-14 13:19:09 +0700

    text form for login

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,7 +1,9 @@
+import 'package:cafe_app/constants/app_color.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../widgets/text_form_widget.dart';
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
@@ -9,15 +11,47 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 47, horizontal: 111),
+                child: Image.asset(
+                  "assets/images/login.png",
+                  height: 149,
+                  width: 149,
+                ),
+              ),
+              const Text(
+                "Login",
+                style: TextStyle(
+                    color: AppColor.primaryColor,
+                    fontFamily: 'Nunito',
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 36, vertical: 40),
+                child: Column(
+                  children: [
+                    TextFormWidget(
+                      icon: Icons.alternate_email,
+                      hintText: "username",
+                    ),
+                    const SizedBox(height: 23),
+                    TextFormWidget(
+                      icon: Icons.lock_outline,
+                      hintText: "password",
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
