c0f4ea2e658e5b9989bfeb23f1cee26751605e0d
dna2121 <dnatayawibisono@gmail.com>
2022-10-18 22:39:27 +0700

    bottom navigation bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,6 @@
+import 'package:cafe_app/constants/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:get/get.dart';
 
@@ -9,20 +11,25 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Obx(
-          () => Text(
-            '${controller.count}',
-            style: TextStyle(fontSize: 20),
-          ),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          onTap: controller.changeTabIndex,
+          currentIndex: controller.tabIndex.value,
+          backgroundColor: AppColor.navbarColor,
+          selectedItemColor: AppColor.primaryColor,
+          unselectedItemColor: AppColor.textColor,
+          items: const [
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.house),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.user),
+              activeIcon: FaIcon(FontAwesomeIcons.solidUser),
+              label: 'Profile',
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => controller.increment(),
       ),
     );
   }
