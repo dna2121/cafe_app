350c50e2131ea9725a511db331382842b0b5173d
dna2121 <dnatayawibisono@gmail.com>
2022-10-20 22:12:59 +0700

    create addProduct function

--- lib/app/modules/add_product/controllers/add_product_controller.dart
+++ lib/app/modules/add_product/controllers/add_product_controller.dart
@@ -3,4 +3,14 @@ import 'package:get/get.dart';
 
 class AddProductController extends GetxController {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void addProduct(String name, int price) {
+    CollectionReference coffee = firestore.collection("coffee");
+
+    coffee.add({
+      "name": name,
+      "price": price,
+    });
+
+  }
 }
