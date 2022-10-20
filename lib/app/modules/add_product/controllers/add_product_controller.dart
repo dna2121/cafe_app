8632b7792d15b7d9b9ad054928d72a3f7960d40d
dna2121 <dnatayawibisono@gmail.com>
2022-10-20 22:18:53 +0700

    add function for initiate and dispose for the controller

--- lib/app/modules/add_product/controllers/add_product_controller.dart
+++ lib/app/modules/add_product/controllers/add_product_controller.dart
@@ -1,7 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class AddProductController extends GetxController {
+  late TextEditingController nameC;
+  late TextEditingController priceC;
+
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   void addProduct(String name, int price) {
@@ -11,6 +15,19 @@ class AddProductController extends GetxController {
       "name": name,
       "price": price,
     });
+  }
 
+  @override
+  void onInit() {
+    nameC = TextEditingController();
+    priceC = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    nameC.dispose();
+    priceC.dispose();
+    super.onClose();
   }
 }
