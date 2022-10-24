9a6a0ec6fc93764062f5f36848344d5c8ae8aa9e
dna2121 <dnatayawibisono@gmail.com>
2022-10-24 23:14:26 +0700

    add default dialog if success or failed

--- lib/app/modules/add_product/controllers/add_product_controller.dart
+++ lib/app/modules/add_product/controllers/add_product_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cafe_app/constants/app_color.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
@@ -11,10 +12,30 @@ class AddProductController extends GetxController {
   void addProduct(String name, int price) {
     CollectionReference coffee = firestore.collection("coffee");
 
-    coffee.add({
-      "name": name,
-      "price": price,
-    });
+    try {
+      coffee.add({
+        "name": name,
+        "price": price,
+      });
+
+      Get.defaultDialog(
+          title: 'Success',
+          middleText: 'Product added.',
+          onConfirm: () {
+            nameC.clear();
+            priceC.clear();
+            Get.back(); //close dialog
+            Get.back(); //back to home
+          },
+          textConfirm: 'Okay',
+          confirmTextColor: const Color.fromARGB(255, 255, 255, 255),
+          buttonColor: AppColor.buttonColor);
+    } catch (e) {
+      Get.defaultDialog(
+        title: 'Failed',
+        middleText: 'Product not added.',
+      );
+    }
   }
 
   @override
