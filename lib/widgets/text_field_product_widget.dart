05912e2baa4a72e5d1ab4c02dde4c65c749c0687
dna2121 <dnatayawibisono@gmail.com>
2022-10-24 23:15:32 +0700

    add keyboardtype and inputaction

--- lib/widgets/text_field_product_widget.dart
+++ lib/widgets/text_field_product_widget.dart
@@ -3,18 +3,25 @@ import 'package:flutter/material.dart';
 import '../constants/app_color.dart';
 
 class TextFieldProduct extends StatelessWidget {
-  const TextFieldProduct({
-    Key? key,
-    required this.labelText, this.controller
-  }) : super(key: key);
+  const TextFieldProduct(
+      {Key? key,
+      required this.labelText,
+      this.controller,
+      this.keyboardType,
+      this.textInputAction})
+      : super(key: key);
 
   final String labelText;
   final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
       decoration: InputDecoration(
         labelText: labelText,
         floatingLabelStyle: const TextStyle(color: AppColor.primaryColor),
