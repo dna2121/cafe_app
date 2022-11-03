4537c290de28f8f7acb9936dce62d552b3c3a290
dna2121 <dnatayawibisono@gmail.com>
2022-11-03 22:47:02 +0700

    listview separated for item card

--- lib/widgets/item_card_widget.dart
+++ lib/widgets/item_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../constants/app_color.dart';
+import '../widgets/text_header_widget.dart';
 
 class ItemCard extends StatelessWidget {
   const ItemCard({
@@ -15,33 +16,56 @@ class ItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(28),
-          child: Image.network(
-            'https://majalah.ottenstatic.com/uploads/2016/09/espresso-013-1024x681.jpg',
-            height: 99,
-            width: 99,
-            fit: BoxFit.cover,
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
+          child: TextHeader(text: "Coffee"),
+        ),
+        SizedBox(
+          height: 160,
+          child: ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            itemCount: 7,
+            separatorBuilder: (context, index) => const SizedBox(),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(28),
+                      child: Image.network(
+                        'https://majalah.ottenstatic.com/uploads/2016/09/espresso-013-1024x681.jpg',
+                        height: 99,
+                        width: 99,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      name,
+                      style: const TextStyle(
+                          fontSize: 12,
+                          color: AppColor.primaryColor,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(height: 7),
+                    Text(
+                      price,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: AppColor.textColor,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ),
-        const SizedBox(height: 5),
-        Text(
-          name,
-          style: const TextStyle(
-              fontSize: 12,
-              color: AppColor.primaryColor,
-              fontWeight: FontWeight.w500),
-        ),
-        const SizedBox(height: 7),
-        Text(
-          price,
-          style: const TextStyle(
-            fontSize: 12,
-            color: AppColor.textColor,
-          ),
-        )
+        const SizedBox(height: 22)
       ],
     );
   }
