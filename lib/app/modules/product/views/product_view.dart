b7ea808a15a3dd9fb263455acae9310e2c670b74
dna2121 <dnatayawibisono@gmail.com>
2022-11-03 21:05:45 +0700

    create logo and apps name

--- lib/app/modules/product/views/product_view.dart
+++ lib/app/modules/product/views/product_view.dart
@@ -1,3 +1,5 @@
+import 'package:cafe_app/constants/app_color.dart';
+import 'package:cafe_app/widgets/primary_text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -11,17 +13,21 @@ class ProductView extends GetView<ProductController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProductView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ProductView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 54, 30, 15),
+        child: Row(
+          children: [
+            Image.network(
+              'https://www.pngplay.com/wp-content/uploads/7/Cafe-Logo-Background-PNG-Image.png',
+              height: 90,
+            ),
+            const SizedBox(width: 15),
+            const PrimaryTextWidget(text: 'Cafe App'),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColor.buttonColor,
         onPressed: () => Get.toNamed(Routes.ADD_PRODUCT),
         child: const FaIcon(FontAwesomeIcons.plus),
       ),
