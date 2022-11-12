71a76b303c006bc3af6685c73bba507ca926e6a2
dna2121 <dnatayawibisono@gmail.com>
2022-11-12 17:17:49 +0700

    add deleteProduct function

--- lib/app/modules/product/controllers/product_controller.dart
+++ lib/app/modules/product/controllers/product_controller.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../../constants/app_color.dart';
 
 class ProductController extends GetxController {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -16,5 +19,31 @@ class ProductController extends GetxController {
     CollectionReference coffee = firestore.collection('coffee');
 
     return coffee.snapshots();
+  }
+
+  void deleteProduct(String docID) {
+    DocumentReference documentReference =
+        firestore.collection("coffee").doc(docID);
+
+    try {
+      Get.defaultDialog(
+          title: "Delete Data",
+          middleText: "Do you want to delete the data?",
+          onConfirm: () async {
+            await documentReference.delete();
+            Get.back();
+          },
+          textConfirm: "Yes",
+          textCancel: "No",
+          confirmTextColor: const Color.fromARGB(255, 255, 255, 255),
+          cancelTextColor: AppColor.buttonColor,
+          buttonColor: AppColor.buttonColor);
+    } catch (e) {
+      // print(e);
+      Get.defaultDialog(
+        title: "Something's wrong.",
+        middleText: "Delete data failed.",
+      );
+    }
   }
 }
