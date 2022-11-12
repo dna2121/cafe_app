737065820e6db34c46dd0d028c91b0bbbc2164b7
dna2121 <dnatayawibisono@gmail.com>
2022-11-12 16:46:59 +0700

    update controller (firebase)

--- lib/app/modules/edit_product/controllers/edit_product_controller.dart
+++ lib/app/modules/edit_product/controllers/edit_product_controller.dart
@@ -1,23 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class EditProductController extends GetxController {
-  //TODO: Implement EditProductController
+import '../../../../constants/app_color.dart';
 
-  final count = 0.obs;
+class EditProductController extends GetxController {
+  late TextEditingController nameC;
+  late TextEditingController priceC;
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<DocumentSnapshot<Object?>> getData(String docID) async {
+    DocumentReference docRef = firestore.collection("coffee").doc(docID);
+    return docRef.get();
+  }
+
+  void editProduct(String name, int price, String docID) {
+    DocumentReference coffee = firestore.collection("coffee").doc(docID);
+
+    try {
+      coffee.update({
+        "name": name,
+        "price": price,
+      });
+
+      Get.defaultDialog(
+          title: 'Success',
+          middleText: 'Product updated.',
+          onConfirm: () {
+            nameC.clear();
+            priceC.clear();
+            Get.back(); //close dialog
+            Get.back(); //back to home
+          },
+          textConfirm: 'Okay',
+          confirmTextColor: const Color.fromARGB(255, 255, 255, 255),
+          buttonColor: AppColor.buttonColor);
+    } catch (e) {
+      Get.defaultDialog(
+        title: 'Failed',
+        middleText: 'Product not updated.',
+      );
+    }
+  }
+
   @override
   void onInit() {
+    nameC = TextEditingController();
+    priceC = TextEditingController();
     super.onInit();
   }
 
   @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
   void onClose() {
+    nameC.dispose();
+    priceC.dispose();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
