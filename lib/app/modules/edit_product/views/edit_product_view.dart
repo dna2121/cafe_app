f5e1bdc756782380e74780074de6372ee49b95d2
dna2121 <dnatayawibisono@gmail.com>
2022-11-12 16:17:54 +0700

    form for edit data

--- lib/app/modules/edit_product/views/edit_product_view.dart
+++ lib/app/modules/edit_product/views/edit_product_view.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../constants/app_color.dart';
+import '../../../../widgets/primary_text_widget.dart';
+import '../../../../widgets/submit_button_widget.dart';
+import '../../../../widgets/text_field_product_widget.dart';
 import '../controllers/edit_product_controller.dart';
 
 class EditProductView extends GetView<EditProductController> {
@@ -9,14 +13,58 @@ class EditProductView extends GetView<EditProductController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('EditProductView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'EditProductView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25, top: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: AppColor.navbarColor,
+                    borderRadius: BorderRadius.circular(11),
+                  ),
+                  child: IconButton(
+                    onPressed: () => Get.back(),
+                    icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                  ),
+                ),
+              ),
+            ),
+            const PrimaryTextWidget(
+              text: 'Edit Product',
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                children: [
+                  const TextFieldProduct(
+                    labelText: 'Name',
+                    // controller: controller.nameC,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  const SizedBox(height: 50),
+                  const TextFieldProduct(
+                    labelText: 'Price',
+                    // controller: controller.priceC,
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.done,
+                  ),
+                  const SizedBox(height: 88),
+                  SubmitButton(
+                    onPressed: () {},
+                    text: "Submit",
+                    width: 249,
+                    height: 49,
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(),
+            const SizedBox(),
+          ],
         ),
       ),
     );
