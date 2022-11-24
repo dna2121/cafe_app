7932220ec59489db12bba01ca113c885725b5e10
dna2121 <dnatayawibisono@gmail.com>
2022-11-24 16:22:26 +0700

    add imagePicker function

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -3,10 +3,12 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileController extends GetxController {
   final storageRef = firebase_storage.FirebaseStorage.instance.ref();
 
+  //file picker
   void uploadFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
 
@@ -24,6 +26,26 @@ class ProfileController extends GetxController {
       }
     } else {
       //user canceled the picker
+    }
+  }
+
+  //image picker
+  void pickImage(ImageSource source) async {
+    final XFile? result = await ImagePicker().pickImage(source: source);
+
+    if (result != null) {
+      File filePath = File(result.path);
+      String fileName = result.name;
+
+      try {
+        await firebase_storage.FirebaseStorage.instance
+            .ref(fileName)
+            .putFile(filePath);
+        Get.snackbar("Success", "File uploaded");
+      } on firebase_storage.FirebaseException catch (e) {
+        //failed
+        print(e);
+      }
     }
   }
 }
