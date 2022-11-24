3901daf774683e52b7014cbe0574e7b7dfaaeeab
dna2121 <dnatayawibisono@gmail.com>
2022-11-24 16:22:03 +0700

    add modalBottomSheet for camera or gallery

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -2,6 +2,7 @@ import 'package:cafe_app/constants/app_color.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../controllers/profile_controller.dart';
 
@@ -18,8 +19,36 @@ class ProfileView extends GetView<ProfileController> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          final ctrl = Get.put(ProfileController());
-          ctrl.uploadFile();
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.camera_alt_rounded),
+                    title: const Text('Camera'),
+                    onTap: () {
+                      final ctrl = Get.put(ProfileController());
+
+                      ctrl.pickImage(ImageSource.camera);
+                      Get.back();
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.photo),
+                    title: const Text('Gallery'),
+                    onTap: () {
+                      final ctrl = Get.put(ProfileController());
+
+                      ctrl.pickImage(ImageSource.gallery);
+                      Get.back();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
         },
         child: const Icon(Icons.upload_rounded),
       ),
