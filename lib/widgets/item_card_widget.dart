5d54fc7a16bd8e1a5d1b4ed9a6b261eb62c43b51
dna2121 <dnatayawibisono@gmail.com>
2022-11-24 16:24:37 +0700

    use listTile for list the products

--- lib/widgets/item_card_widget.dart
+++ lib/widgets/item_card_widget.dart
@@ -21,58 +21,67 @@ class ItemCard extends StatelessWidget {
           child: TextHeader(text: "Coffee"),
         ),
         SizedBox(
-          height: 160,
+          height: 660,
           child: StreamBuilder<QuerySnapshot<Object?>>(
             stream: controller.streamData(),
             builder: (context, snapshot) {
               var listAllData = snapshot.data!.docs;
 
               if (snapshot.connectionState == ConnectionState.active) {
-                return ListView.separated(
+                return ListView.builder(
                   physics: const BouncingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
                   itemCount: listAllData.length,
-                  separatorBuilder: (context, index) => const SizedBox(),
                   itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child: InkWell(
-                        onTap: () => Get.toNamed(Routes.EDIT_PRODUCT,
-                            arguments: listAllData[index].id),
-                        onLongPress: () =>
-                            controller.deleteProduct(listAllData[index].id),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(28),
-                              child: Image.network(
-                                'https://majalah.ottenstatic.com/uploads/2016/09/espresso-013-1024x681.jpg',
-                                height: 99,
-                                width: 99,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              "${(listAllData[index].data() as Map<String, dynamic>)["name"]}",
-                              style: const TextStyle(
-                                  fontSize: 12,
-                                  color: AppColor.primaryColor,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            const SizedBox(height: 7),
-                            Text(
-                              "Rp. ${(listAllData[index].data() as Map<String, dynamic>)["price"]}",
-                              style: const TextStyle(
-                                fontSize: 12,
-                                color: AppColor.textColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                    return ListTile(
+                      title: Text(
+                          "${(listAllData[index].data() as Map<String, dynamic>)["name"]}"),
+                      subtitle: Text(
+                          "Rp. ${(listAllData[index].data() as Map<String, dynamic>)["price"]}"),
+                      leading: const CircleAvatar(),
+                      onTap: () => Get.toNamed(Routes.EDIT_PRODUCT,
+                          arguments: listAllData[index].id),
+                      onLongPress: () =>
+                          controller.deleteProduct(listAllData[index].id),
                     );
+                    // return Padding(
+                    //   padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+                    //   child: InkWell(
+                    //     onTap: () => Get.toNamed(Routes.EDIT_PRODUCT,
+                    //         arguments: listAllData[index].id),
+                    //     onLongPress: () =>
+                    //         controller.deleteProduct(listAllData[index].id),
+                    //     child: Column(
+                    //       crossAxisAlignment: CrossAxisAlignment.start,
+                    //       children: [
+                    //         ClipRRect(
+                    //           borderRadius: BorderRadius.circular(28),
+                    //           child: Image.network(
+                    //             'https://majalah.ottenstatic.com/uploads/2016/09/espresso-013-1024x681.jpg',
+                    //             height: 99,
+                    //             width: 99,
+                    //             fit: BoxFit.cover,
+                    //           ),
+                    //         ),
+                    //         const SizedBox(height: 5),
+                    //         Text(
+                    //           "${(listAllData[index].data() as Map<String, dynamic>)["name"]}",
+                    //           style: const TextStyle(
+                    //               fontSize: 12,
+                    //               color: AppColor.primaryColor,
+                    //               fontWeight: FontWeight.w500),
+                    //         ),
+                    //         const SizedBox(height: 7),
+                    //         Text(
+                    //           "Rp. ${(listAllData[index].data() as Map<String, dynamic>)["price"]}",
+                    //           style: const TextStyle(
+                    //             fontSize: 12,
+                    //             color: AppColor.textColor,
+                    //           ),
+                    //         ),
+                    //       ],
+                    //     ),
+                    //   ),
+                    // );
                   },
                 );
               }
